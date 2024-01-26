f78e7570fa0b011e4a932cb628b0c7ba5ae56492
reinexxism <lucy0805@naver.com>
2024-01-26 15:39:59 +0900

    :lipstick: Page container bgc 변경
    update style

--- src/styles/PageStyle.jsx
+++ src/styles/PageStyle.jsx
@@ -2,12 +2,13 @@ import styled from "styled-components";
 
 const PageStyle = styled.div`
   position: relative;
-  width: 34%;
+  width: 32%;
   height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: white;
+  background-color: #f0f0f0;
+  overflow: hidden;
 `;
 
 export default PageStyle;
