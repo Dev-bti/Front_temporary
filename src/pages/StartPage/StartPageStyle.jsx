a8c95e2f4f8ca1295e0e906827ba26257ae0b83e
reinexxism <lucy0805@naver.com>
2024-01-26 15:00:21 +0900

    :lipstick: StartPage 스타일 컴포넌트 파일 추가
    add startpage style component file

---
+++ src/pages/StartPage/StartPageStyle.jsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+const Jaeseung = styled.img`
+  position: absolute;
+  width: 200px;
+  height: 200px;
+`;
+
+const Joohyun = styled.img`
+  position: absolute
+  width: 200px;
+  height: 200px;
+`;
+
+const MainTitle = styled.div`
+  width: 80%;
+  height: 15%;
+  background-color: black;
+`;
+
+const Title = styled.p`
+  font-size: 1.5rem;
+`;
+
+const Description = styled.p`
+  font-size: 15px;
+`;
