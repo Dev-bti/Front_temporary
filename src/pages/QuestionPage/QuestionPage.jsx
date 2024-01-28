797d0851e91c9025b3d571769c49a2709c990ba0
reinexxism <lucy0805@naver.com>
2024-01-28 15:35:22 +0900

    :art: QuestionPage 컴포넌트 import
    import component

--- src/pages/QuestionPage/QuestionPage.jsx
+++ src/pages/QuestionPage/QuestionPage.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import PageContainer from "../../styles/PageContainer";
 import PageStyle from "../../styles/PageStyle";
+import ProgressBar from "../../components/ProgressBar";
+import ProgressIndex from "../../components/ProgressIndex";
 
 export default function QuestionPage() {
   return (
     <PageContainer>
       <PageStyle>
+        <ProgressBar />
+        <ProgressIndex />
         <h1>여기는 질문 페이지입니다.</h1>
         <Link to="/result">
           <button>다음</button>
