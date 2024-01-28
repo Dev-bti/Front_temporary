5b07fe836d7921f3a7ccbaefa158a0e09bf35d3f
reinexxism <lucy0805@naver.com>
2024-01-28 16:03:13 +0900

    :art: ProgressBar, ProgressIndex 컴포넌트 추가
    add component

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,8 +1,28 @@
-import React, { useState } from "react";
-import QuestionPageProgressBar from "../styles/ProgressBarStyle";
+import React from "react";
+import styled from "styled-components";
 
-export default function ProgressBar() {
-  const [curProgress, setCurProgress] = useState(10);
+const ProgressContainer = styled.div`
+  width: 80%;
+  height: 4px;
+  margin-top: 50px;
+  background-color: #e1e1e1;
+  border-radius: 10px;
+`;
 
-  return <QuestionPageProgressBar progress={curProgress} />;
-}
+const Progress = styled.div`
+  width: ${(props) => props.progress}%;
+  height: 100%;
+  background-color: #62abbb;
+  border-radius: 10px;
+  transition: width 0.3s ease;
+`;
+
+const ProgressBar = ({ progress }) => {
+  return (
+    <ProgressContainer>
+      <Progress progress={progress} />
+    </ProgressContainer>
+  );
+};
+
+export default ProgressBar;
--- src/components/ProgressIndex.jsx
+++ src/components/ProgressIndex.jsx
@@ -1,11 +1,23 @@
-import React, { useState } from "react";
-import { EntireIndex, CurIndex } from "../styles/ProgressIndexStyle";
+import React from "react";
+import styled from "styled-components";
 
-export default function ProgressIndex() {
-  const [curIndex, setCurIndex] = useState(1);
+const EntireIndex = styled.p`
+  margin-top: 30px;
+  color: #252525;
+  font-size: 1.3rem;
+`;
+
+const CurIndex = styled.span`
+  font-weight: bold;
+  color: #7bb0bc;
+`;
+
+const ProgressIndex = ({ curIndex, totalIndex }) => {
   return (
     <EntireIndex>
-      <CurIndex>{curIndex}</CurIndex> / 10
+      <CurIndex>{curIndex}</CurIndex> / {totalIndex}
     </EntireIndex>
   );
-}
+};
+
+export default ProgressIndex;
