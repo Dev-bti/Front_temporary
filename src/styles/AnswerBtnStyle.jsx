d1a2e9048284b24eda25051728f0643aa4e3280a
reinexxism <lucy0805@naver.com>
2024-01-28 16:04:11 +0900

    :fire: Remove deprecrated styled-components
    delete

--- src/styles/AnswerBtnStyle.jsx
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-const AnswerBtnStyle = {};
-
-export default AnswerBtnStyle;
--- src/styles/ProcessBtnStyle.jsx
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-const ProcessBtnStyle = {};
-
-export default ProcessBtnStyle;
