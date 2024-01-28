9d27907dc1a2488001de6cf64b728158d881c4f3
reinexxism <lucy0805@naver.com>
2024-01-28 17:33:05 +0900

    :art: QuestionPage 질문지 컴포넌트 import
    add component

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,6 +1,51 @@
 import React from "react";
-import QuestionStyle from "../styles/QuestionStyle";
+import styled from "styled-components";
 
-export default function Question() {
-  return <div></div>;
-}
+const QuestionContainer = styled.div`
+  position: relative;
+  width: 80%;
+  height: 38%;
+  margin-top: 15px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const QuestionImage = styled.img`
+  width: 60%;
+  height: 66%;
+  margin-top: 10px;
+`;
+
+const QuestionText = styled.div`
+  position: absolute;
+  top: 71%;
+  width: 90%;
+  height: 23%;
+  display: flex;
+  align-items: center;
+  margin-top: 5px;
+  text-align: center;
+  line-height: 25px;
+  font-size: 15px;
+`;
+
+const BorderLine = styled.div`
+  position: absolute;
+  width: 100%;
+  border: 1px solid white;
+  top: 96%;
+`;
+
+const Question = ({ text, img }) => {
+  return (
+    <QuestionContainer>
+      {img && <QuestionImage src={img} />}
+      {/* <QuestionImage src={img} /> */}
+      <QuestionText>{text}</QuestionText>
+      <BorderLine />
+    </QuestionContainer>
+  );
+};
+
+export default Question;
