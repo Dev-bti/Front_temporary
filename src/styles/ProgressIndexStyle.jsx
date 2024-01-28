b9fb6be5448492fbe7df965e501776d02b585376
reinexxism <lucy0805@naver.com>
2024-01-28 15:33:53 +0900

    :lipstick: QuestionPage 질문 인덱스 넘버 스타일 추가
    add style

---
+++ src/styles/ProgressIndexStyle.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const EntireIndex = styled.p`
+  margin-top: 30px;
+  color: #252525;
+  font-size: 1.3rem;
+`;
+
+export const CurIndex = styled.span`
+  font-weight: bold;
+  color: #7bb0bc;
+`;
