254b001f8e1dbd79bbd3b03e53d0dbfd25b6d4f4
reinexxism <lucy0805@naver.com>
2024-01-29 23:48:14 +0900

    :lipstick: PageContainer background-color 변경
    change

--- src/styles/PageContainer.jsx
+++ src/styles/PageContainer.jsx
@@ -6,7 +6,7 @@ const PageContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: #252525;
+  background-color: #f0f0f0;
 `;
 
 export default PageContainer;
