3c5321ea6ba53a204e29f9472aa1c77d1720e5d6
reinexxism <lucy0805@naver.com>
2024-01-29 14:47:50 +0900

    :art: Import AnswerBtn component
    import

--- src/pages/QuestionPage/QuestionPage.jsx
+++ src/pages/QuestionPage/QuestionPage.jsx
@@ -5,11 +5,17 @@ import PageStyle from "../../styles/PageStyle";
 import ProgressIndex from "../../components/ProgressIndex";
 import ProgressBar from "../../components/ProgressBar";
 import Question from "../../components/Question";
+import AnswerBtn from "../../components/AnswerBtn";
 
 export default function QuestionPage() {
   const [curQuestion, setCurQuestion] = useState({
     text: "여기는 질문이 들어갈 영역입니다.여기는 질문이 들어갈 영역입니다.여기는 질문이 들어갈 영역입니다.",
     img: "src/assets/question/question_calculator.png",
+    answer: [
+      "1. 친구 자취방의 분위기와 어울릴만한 인테리어 소품",
+      "2. 자취에 필요한 화장지나 수건 같은 실용적인 물건",
+      "3. 히히히히히히히 하하하하하하 헤헤헤헤헤헤호호",
+    ],
   });
   const [curQuestionIndex, setCurQuestionIndex] = useState(1);
   const totalQuestions = 10;
@@ -44,6 +50,7 @@ export default function QuestionPage() {
           text={curQuestion.text}
           img={curQuestion.img ? curQuestion.img : null}
         />
+        <AnswerBtn items={curQuestion.answer} />
         <Link to="/result">
           <button>다음</button>
         </Link>
