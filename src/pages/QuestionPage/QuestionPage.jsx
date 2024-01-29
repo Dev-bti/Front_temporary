32313cdbad2acb6eac8cadf7354d965ba9dbe7a5
reinexxism <lucy0805@naver.com>
2024-01-29 16:58:51 +0900

    :art: QuestionPage 네크워킹 통신 테스트 성공
    fetch complete

--- src/pages/QuestionPage/QuestionPage.jsx
+++ src/pages/QuestionPage/QuestionPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import PageContainer from "../../styles/PageContainer";
 import PageStyle from "../../styles/PageStyle";
@@ -6,38 +6,43 @@ import ProgressIndex from "../../components/ProgressIndex";
 import ProgressBar from "../../components/ProgressBar";
 import Question from "../../components/Question";
 import AnswerBtn from "../../components/AnswerBtn";
+import ProcessBtn from "../../components/ProcessBtn";
 
 export default function QuestionPage() {
-  const [curQuestion, setCurQuestion] = useState({
-    text: "여기는 질문이 들어갈 영역입니다.여기는 질문이 들어갈 영역입니다.여기는 질문이 들어갈 영역입니다.",
-    img: "src/assets/question/question_calculator.png",
-    answer: [
-      "1. 친구 자취방의 분위기와 어울릴만한 인테리어 소품",
-      "2. 자취에 필요한 화장지나 수건 같은 실용적인 물건",
-      "3. 히히히히히히히 하하하하하하 헤헤헤헤헤헤호호",
-    ],
-  });
+  const [entireQuestion, setEntireQuestion] = useState([]);
+  const [curQuestion, setCurQuestion] = useState(null);
   const [curQuestionIndex, setCurQuestionIndex] = useState(1);
   const totalQuestions = 10;
 
   const progress = (curQuestionIndex / totalQuestions) * 100;
 
-  // 다음 질문으로 가는 버튼(n.1-9)
+  useEffect(() => {
+    fetch("src/data/dummy-data_question.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setEntireQuestion(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    const foundQuestion = entireQuestion.find(
+      (question) => question.question_ID === curQuestionIndex
+    );
+    setCurQuestion(foundQuestion);
+  }, [curQuestionIndex, entireQuestion]);
+
   const handleNextBtn = () => {
     if (curQuestionIndex < totalQuestions) {
       setCurQuestionIndex((prev) => prev + 1);
     }
   };
-  // 이전 질문으로 가는 버튼(n.2-10)
+
   const handlePrevBtn = () => {
     if (curQuestionIndex > 0) {
       setCurQuestionIndex((prev) => prev - 1);
     }
   };
-  // 결과 화면으로 가는 버튼(n.10)
-  const handleResultBtn = () => {
-    <Link to="/result" />;
-  };
+
   return (
     <PageContainer>
       <PageStyle>
@@ -47,13 +52,14 @@ export default function QuestionPage() {
           totalIndex={totalQuestions}
         />
         <Question
-          text={curQuestion.text}
-          img={curQuestion.img ? curQuestion.img : null}
+          questionNum={curQuestion?.question_ID}
+          text={curQuestion?.question_Sentence}
         />
-        <AnswerBtn items={curQuestion.answer} />
-        <Link to="/result">
-          <button>다음</button>
-        </Link>
+        <AnswerBtn items={curQuestion?.answers} />
+        <ProcessBtn
+          questionID={curQuestion?.question_ID}
+          onClickFunction={[handleNextBtn, handlePrevBtn]}
+        />
       </PageStyle>
     </PageContainer>
   );
