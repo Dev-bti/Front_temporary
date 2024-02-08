f731c31570e853a20897b8ea5e7028c1fe42e89e
reinexxism <lucy0805@naver.com>
2024-02-08 17:07:36 +0900

    :art: Modify: Import components + code refactoring
    .

--- src/pages/ResultPage/ResultPage.jsx
+++ src/pages/ResultPage/ResultPage.jsx
@@ -4,17 +4,19 @@ import PageContainer from "../../styles/PageContainer";
 import PageStyle from "../../styles/PageStyle";
 import ResultTitle from "../../components/ResultTitle";
 import ResultText from "../../components/ResultText";
+import ResultBtn from "../../components/ResultBtn";
 
 export default function ResultPage() {
   return (
     <PageContainer>
       <PageStyle>
-        <ResultTitle />
-        <ResultText />
-        <ResultText />
-        <Link to="/">
-          <button>처음으로</button>
-        </Link>
+        <ResultTitle
+          adjective="감각적인"
+          result="프론트엔드 개발자"
+          resultId="1"
+        />
+        <ResultText type="프론트엔드 개발자" />
+        <ResultBtn />
       </PageStyle>
     </PageContainer>
   );
