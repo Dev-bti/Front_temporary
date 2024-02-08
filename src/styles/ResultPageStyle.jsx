e8191e07693ee5aaca88a3f6b5157b9f61c65591
reinexxism <lucy0805@naver.com>
2024-02-08 17:06:58 +0900

    :lipstick: Modify: Add ResultPage style
    change

---
+++ src/styles/ResultPageStyle.jsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+export const Border = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: #d6d6d6;
+`;
+
+export const Span = styled.span`
+  font-weight: bold;
+  color: #37a5ad;
+`;
+
+export const TitleText = styled.p`
+  margin: 15px 0;
+  font-size: 1rem;
+  color: #606060;
+`;
+
+export const Ul = styled.ul`
+  margin-top: 20px;
+  margin-bottom: 60px;
+  width: 100%;
+  text-align: left;
+`;
+
+export const Li = styled.li`
+  width: 100%;
+  margin-bottom: 25px;
+  list-style-type: none;
+  color: #464646;
+  background-color: white;
+`;
+
+export const Lang = styled.img`
+  width: 100%;
+  margin-bottom: 10px;
+  height: 90px;
+`;
+
+export const Lecture = styled.img`
+  width: 80%;
+  height: 150px;
+  margin-bottom: 10px;
+`;
